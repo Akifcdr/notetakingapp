78c1b599a92c62bdcfe0435d55c62e382065e9d8
Akif Abdullah <akifabdullah199@gmail.com>
2025-09-02 19:33:13 +0530

    modified files and added PasswordInput file

--- frontend/notes-app/tailwind.config.js
+++ frontend/notes-app/tailwind.config.js
@@ -4,7 +4,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#2B85ff",
+        secondary: "#EF863E",
+      },
+    },
   },
   plugins: [],
 }
