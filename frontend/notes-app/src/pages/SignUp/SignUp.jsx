1e195b7f5de335296d505e36f3fd850b68fee292
Akif Abdullah <akifabdullah199@gmail.com>
2025-09-04 23:19:52 +0530

    updated signup

--- frontend/notes-app/src/pages/SignUp/SignUp.jsx
+++ frontend/notes-app/src/pages/SignUp/SignUp.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
-
+import Navbar from '../../components/Navbar/Navbar'
 const SignUp = () => {
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+  };
+  
   return (
-    <div>
-      SignUp Page
-    </div>
+    <>
+      <Navbar/>
+      <div className='flex items-center justify-center mt-28'>
+        <div className='w-96 border rounded bg-white px-7 py-10'>
+          <form onSubmit={handleSignUp}>
+            <h4 className="text-2xl mb-7">SignUp</h4>
+            <input 
+              type="text" 
+              placeholder="Email" 
+              className="input-box" 
+              value={email}
+              onChange={(e)=>setEmail(e.target.value)}
+              />
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
 
