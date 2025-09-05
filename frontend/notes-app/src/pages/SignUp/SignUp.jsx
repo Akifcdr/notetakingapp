0927114aca21aa167719b4969684426d617d08dd
Akif Abdullah <akifabdullah199@gmail.com>
2025-09-05 23:43:04 +0530

    Signup UI updated

--- frontend/notes-app/src/pages/SignUp/SignUp.jsx
+++ frontend/notes-app/src/pages/SignUp/SignUp.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React,{ useState } from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import { Link } from 'react-router-dom'
+import PasswordInput from '../../components/input/passwordinput'
 const SignUp = () => {
+
+  const [name,setName] = useState("");
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
+  const [error,setError] = useState(null);
   const handleSignUp = async (e) => {
     e.preventDefault();
+
+    if(!name){
+      setError("Please enter name");
+      return;
+    }
+    if(!validateEmail(email)){
+      setError("Please enter a valid email address");
+      return;
+    }
+    if(!password){
+      setError("Please enter password");
+      return;
+    }
+    setError('');
   };
-  
+
   return (
     <>
       <Navbar/>
@@ -14,11 +35,32 @@ const SignUp = () => {
             <h4 className="text-2xl mb-7">SignUp</h4>
             <input 
               type="text" 
+              placeholder="Name" 
+              className="input-box" 
+              value={name}
+              onChange={(e)=>setName(e.target.value)}
+              />
+            <input 
+              type="text" 
               placeholder="Email" 
               className="input-box" 
               value={email}
               onChange={(e)=>setEmail(e.target.value)}
               />
+            <PasswordInput
+              value={password}
+              onChange={(e)=>setPassword(e.target.value)}
+            />
+            {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
+            <button type="submit" className="btn-primary">
+              Create Account
+            </button>
+            <p className="text-sm text-center mt-4">
+              Already have an Account?{" "}
+              <Link to="/login" className="font-medium text-primary underline">
+                Login
+              </Link>
+            </p>
           </form>
         </div>
       </div>
