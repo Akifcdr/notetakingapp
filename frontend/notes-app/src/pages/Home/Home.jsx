f8a05b1f17cb5a96614a1057596e8643895725bc
Akif Abdullah <akifabdullah199@gmail.com>
2025-09-07 23:37:44 +0530

    added profileInfo import

--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
-
+import profileInfo from '../../components/cards/profileInfo'
 const Home = () => {
   return (
     <>
