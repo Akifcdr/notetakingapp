f30855fb5ed5f0659ba467b835dfcfa21fe5ed0f
Akif Abdullah <akifabdullah199@gmail.com>
2025-09-09 23:22:55 +0530

    updated navbar for searchbar and profileInfo

--- frontend/notes-app/src/components/Navbar/Navbar.jsx
+++ frontend/notes-app/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import ProfileInfo from '../cards/ProfileInfo'
+import { useNavigate } from 'react-router-dom';
+import Searchbar from '../SearchBar/Searchbar';
 const Navbar = () => {
+  const navigate = useNavigate();
+
+  const onLogout = () => {
+    navigate('/login');
+  }
   return (
     <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
         <h2 className="text-xl font-medium text-black py-2">Notes</h2>
-        
-        <ProfileInfo/>
+        <Searchbar/>
+        <ProfileInfo onLogout={onLogout}/>
     </div>
   )
 }
