f54036471e01d6cb16ae3870d655dd35e7fe7178
Akif Abdullah <akifabdullah199@gmail.com>
2025-09-10 23:10:58 +0530

    updated SearchBar

--- frontend/notes-app/src/components/SearchBar/Searchbar.jsx
+++ frontend/notes-app/src/components/SearchBar/Searchbar.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
-const Searchbar = () => {
+const Searchbar = ({value,onChange,handleSearch,onClearSearch}) => {
   return (
-    <div>
-      SearchBar
+    <div className="w-80 flex-items-center px-4 bg-slate-100 rounded-md">
+      <input 
+      type="text"
+      placeholder='Search Notes'
+      className='w-full text-xs bg-transparent py-[11px] outline-none'
+      value='value'
+      onChange={onChange}
+      />
+      
     </div>
   )
 }
