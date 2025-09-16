233e8a23c94ff6300368d29f1fce3fcb4186772c
Akif Abdullah <akifabdullah199@gmail.com>
2025-09-16 22:14:27 +0530

    imported icons and added css

--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,22 +1,28 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import NoteCard from '../../components/cards/NoteCard'
+import { MdAdd } from 'react-icons/md'
 const Home = () => {
   return (
     <>
       <Navbar/>
       <div className="container-mx-auto">
-        <NoteCard
-          title="Meeting on 27th Jan"
-          date="27th Jan, 2024"
-          content="Meeting on 27th Jan Meeting on 27th Jan"
-          tags="#Meeting"
-          isPinned={true}
-          onEdit={() => {}}
-          onDelete={() => {}}
-          onPinNote={() => {}}
-        />
+        <div className='grid grid-cols-3 gap-4 mt-8'>
+          <NoteCard
+            title="Meeting on 27th Jan"
+            date="27th Jan, 2024"
+            content="Meeting on 27th Jan Meeting on 27th Jan"
+            tags="#Meeting"
+            isPinned={true}
+            onEdit={() => {}}
+            onDelete={() => {}}
+            onPinNote={() => {}}
+            />
+        </div>
       </div>
+      <button className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10" onClick={()=>{}}>
+        <MdAdd className='text-[32px] text-white'/>
+      </button>
     </>
   )
 }
