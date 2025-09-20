ad0dbca813b4f361ea0c1cc082bead624fe2b0c5
Akif Abdullah <akifabdullah199@gmail.com>
2025-09-20 21:26:22 +0530

    tags file updated

--- frontend/notes-app/src/components/Input/TagInput.jsx
+++ frontend/notes-app/src/components/Input/TagInput.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { MdAdd,MdClose } from 'react-icons/md'
 const TagInput = ({tags,setTags}) => {
+  const [inputValue,setInputValue]=useState("");
+  const handleInputChange=(e)=>{
+    setInputValue(e.target.value);
+  }
+  const addNewTag=()=>{
+    if(inputValue.trim()!==""){
+      setTags([...tags,inputValue.trim()]);
+      setInputValue("");
+    }
+  }
   return (
     <div>
       <div className='flex items-center gap-4 mt-3'>
-        <input type='text' className='' placeholder='Add tags here...'/>
-        <button className=''>
+        <input type='text' 
+        className='text-sm bg-transparent border px-3 py-2 rounded outline-none' 
+        placeholder='Add tags here...'
+        onChange={handleInputChange}/>
+        <button className='w-8 h-8 flex items-center justify-center rounded border border-blue-700 hover:bg-blue-700'>
             <MdAdd className='text-2xl text-blue-700 hover:text-white'/>
         </button>
       </div>
