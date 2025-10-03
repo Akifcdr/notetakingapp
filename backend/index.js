e00a5e08ef93e3fe57ad2fb7159a95d9c3fb3cdc
Akif Abdullah <akifabdullah199@gmail.com>
2025-10-03 15:29:54 +0530

    updated for create account and mongoose connection

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,16 @@
+require("dotenv").config();
+
+const config=require("./config.json");
+const mongoose=require("mongoose");
+
+mongoose.connect(config.connectionString);
+
 const express=require('express');
 const cors=require("cors");
 const app=express();
+
+const jwt=require('jsonwebtoken');
+const {authenticateToken}=require("./utilities");
 
 app.use(express.json());
 app.use(
@@ -10,6 +20,22 @@ app.use(
 );
 app.get("/",(req,res)=>{
     res.json({data:"hello"});
+});
+
+//create Account
+app.post("/create-account",async(req,res)=>{
+    const {fullName,email,password}=req.body;
+    if(!fullName){
+        return res
+        .status(400)
+        .json({error:true,message:"Full Name is required"});
+    }
+    if(!email){
+        return res.status(400).json({error:true,message:"Email is required"});
+    }
+    if(!password){
+        return res.status(400).json({error:true,message:"Password is required"});
+    }
 });
 app.listen(8000);
 
