df265cca69cf571972832624b58e44809054f5e7
Akif Abdullah <akifabdullah199@gmail.com>
2025-10-05 18:34:10 +0530

    updated index.js for userlogin and authentication

--- backend/index.js
+++ backend/index.js
@@ -38,6 +38,9 @@ app.post("/create-account",async(req,res)=>{
     if(!password){
         return res.status(400).json({error:true,message:"Password is required"});
     }
+
+    const isUser=await User.findOne({email:email});
+    
     if(isUser){
         return res.json({
             error:true,
@@ -62,6 +65,39 @@ app.post("/create-account",async(req,res)=>{
         message:"Account created successfully",
     });
 }); 
+
+app.post("/login",async(req,res)=>{
+    const {email,password}=req.body;
+    if(!email){
+        return res.status(400).json({message:"Email is required"});
+    }
+    if(!password){
+        return res.status(400).json({message:"Password is required"});
+    }
+    const userInfo=await User.findOne({email:email});
+    if(!userInfo){
+        return res
+        .status(400)
+        .json({message:"User not found"});
+    }
+    if(userInfo.email==email && userInfo.password==password){
+        const user={user:userInfo};
+        const accessToken=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
+            expiresIn:'36000m',
+        });
+        return res.json({
+            error:false,
+            message:"Login successful",
+            email,
+            accessToken,
+        });
+    }else{
+        return res
+        .status(400)
+        .json({error:true,message:"Invalid credentials"});
+    }
+});
+    
 app.listen(8000);
 
 module.exports=app;
