5c46dc7397fb80ada99b6324d19f5f3b43ca8d28
Akif Abdullah <akifabdullah199@gmail.com>
2025-10-09 22:34:06 +0530

    updated index.js for editnotes functionality

--- backend/index.js
+++ backend/index.js
@@ -104,7 +104,6 @@ app.post("/addNote",authenticateToken,async(req,res)=>{
     const {user}=req.user;
     if(!title){
         return res
-        
         .status(400)
         .json({error:true,message:"Title is required"});
     }
@@ -127,6 +126,39 @@ app.post("/addNote",authenticateToken,async(req,res)=>{
             message:"Note added successfully",
         });
     }catch(err){
+        return res
+        .status(500)
+        .json({error:true,message:"Internal server error"});
+    }
+});
+app.put("/editNote/:noteId",authenticateToken,async(req,res)=>{
+    const noteId=req.params;
+    const {title,content,tags,isPinned}=req.body;
+    const {user}=req.user;
+    if(!title&&!content&&!tags){
+        return res
+        .status(400)
+        .json({error:true,message:"No changes provided"});
+    }
+    try{
+        const note=await Note.findOne({_id:noteId,userId:user._id});
+        if(!note){
+            return res
+            .status(404)
+            .json({error:true,message:"Note not found"});
+        }
+        if(title) note.title=title;
+        if(content) note.content=content;
+        if(tags) note.tags=tags;
+        if(isPinned) note.isPinned=isPinned;
+
+        await note.save();
+        return res.json({
+            error:false,
+            note,
+            message:"Note updated successfully",
+        });
+    }catch(error){
         return res
         .status(500)
         .json({error:true,message:"Internal server error"});
