866eb401ca8e9a8b7450ef01f4adab3b0e236f69
Akif Abdullah <akifabdullah199@gmail.com>
2025-10-10 22:45:41 +0530

    upadted index.js for getallnotes function

--- backend/index.js
+++ backend/index.js
@@ -164,6 +164,17 @@ app.put("/editNote/:noteId",authenticateToken,async(req,res)=>{
         .json({error:true,message:"Internal server error"});
     }
 });
+app.get("/get-all-notes/",authenticateToken,async(req,res)=>{
+    const {user}=req.user;
+    try{
+        const notes=await Note.find({userId:user._id}).sort({isPinned:-1});
+        return res.json({error:false,notes,message:"Notes fetched successfully"});
+    }catch(error){
+        return res
+        .status(500)
+        .json({error:true,message:"Internal server error"});
+    }
+});
 app.listen(8000);
 
 module.exports=app;
