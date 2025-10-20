27689a6a6b214d79b8e77ab979641e8d2093bd9d
Akif Abdullah <akifabdullah199@gmail.com>
2025-10-20 23:21:44 +0530

    updated profileInfo for dynamic userInfo display

--- frontend/notes-app/src/components/cards/profileInfo.jsx
+++ frontend/notes-app/src/components/cards/profileInfo.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { getInitials } from '../../utils/helper'
-const profileInfo = ({onLogout}) => {
+const profileInfo = ({userInfo,onLogout}) => {
   return (
     <div className="flex items-center gap-3">
       <div className="w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100">
-        {getInitials("Akif Abdullah")}
+        {getInitials(userInfo?.fullName)}
       </div>
       <div>
-        <p className="text-sm font-medium">Akif Abdullah</p>
-        <button className="text-sm text-slate-700 underline" onClick={onLogout}>
+        <p className="text-sm font-medium">{userInfo.fullName}</p>
+        <button 
+          className="text-sm text-slate-700 underline" 
+          onClick={onLogout}
+        >
           Logout
         </button>
       </div>  
